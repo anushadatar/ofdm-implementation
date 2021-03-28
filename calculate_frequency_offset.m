532bc9941294a1473f9abdd7efcede4fe52a3412
Mark Goldwater <mgoldwater@olin.edu>
2021-03-28 15:23:27 -0400

    Fixed division error in frequency estimation

--- calculate_frequency_offset.m
+++ calculate_frequency_offset.m
@@ -11,12 +11,10 @@ function f_delta_hat = calculate_frequency_offset(y)
         exponentials(i-offset+1) = angle(y(i+offset-17) ./ y(i));
     end
     
-    length(exponentials)
-    
-    test = mean(exponentials)./64
+    f_delta_hat = mean(exponentials)./80;
     
     % Take angle of all exponents and divide by 64
-    f_delta_hat = 0.032 %(1/64).*sum(exponentials)
+    %f_delta_hat = 0.032 %(1/64).*sum(exponentials)
     
     % Return corrected signal
     % exp_vector = exp(-1i*f_delta_hat*[1:length(y_fft)]);
