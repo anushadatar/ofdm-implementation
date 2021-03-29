31306872bcd7d959198df8cb7af6f804c21b9e6c
Anusha Datar <anushadatar@gmail.com>
2021-03-28 23:29:42 -0400

    parameterized and documented transmit side

--- package_data.m
+++ package_data.m
@@ -1,18 +1,26 @@
-function tx_cyclic = package_data(x_train, x_data, block_size, prefix_size)
-    % Package the existing transmit data
+function tx_cyclic = package_data(x_train, x_data, block_size, prefix_size, num_preamb_blocks)
+    % Package the existing transmit data to include the encoded training
+    % data, encoded data to send, and a preamble.
     % Input Parameters:
-    % x_train     : The training signal to include in the data (including
-    %               any additional preamble information).
-    % x_data      : The data to transmit after the training signal.
-    % block_size  : The number of bits per block (64 for wifi).
-    % prefix_size : The number of bits in the prefix (16 for wifi).
+    % x_train           : The training signal to include in the data.
+    % x_data            : The data to transmit after the training signal.
+    % block_size        : The number of bits per block (64 for wifi).
+    % prefix_size       : The number of bits in the prefix (16 for wifi).
+    % num_preamb_blocks : The number of blocks of preamble data to include. 
     % Returns:
-    % tx_cyclic   : Packaged transmit data with training signal and cyclic
-    %               prefix encoding with ifft taken.
+    % tx_cyclic         : Packaged transmit data with the preamble followed
+    %                     by the encoded and iffted training data and data
+    %                     to transmit over the channel.
 
     % Construct tx signal by appending the training signal to the beginning
     % of the data signal
     tx = [x_train x_data];
     % Add cyclic prefixes and take the ifft
     tx_cyclic = encode_data(tx, block_size, prefix_size);
+    
+    % Create preamble and add it to the prepared signal
+    preamb_block = sign(randn(1,block_size)) + sign(randn(1,block_size))*1i;
+    preamb = repmat(preamb_block, 1, num_preamb_blocks);
+    tx_cyclic = [preamb tx_cyclic];
+
 end
--- simulate_without_synchronized_clocks.m
+++ simulate_without_synchronized_clocks.m
@@ -1,29 +1,27 @@
 function [output_data, error_rate] = simulate_without_synchronized_clocks(x_train, x_data)
-    % Simulate sending and receiving data using OFDM.
-    % x_train : The training signal to transmit across the channel.
-    % x_data  : The data to transmit across the channel.
+    % Simulate sending and receiving data using OFDM across a channel that
+    % introduces a frequency offset. 
+    % x_train     : The training signal to transmit across the channel.
+    % x_data      : The data to transmit across the channel.
     % Returns:
     % output_data : The simulated decoded data.
     % error_rate  : The percent error between the simulated decoded data
-    % and the original transmit data.
+    %               and the original transmit data.
     
     % System parameters
-    number_of_blocks = 100;
-    block_size = 64;
-    prefix_size = 16;
-    num_train = 30;
-    
+    number_of_blocks = 100;  % Number of blocks in data sequence
+    block_size = 64;         % Bits per block (not including prefix)
+    prefix_size = 16;        % Prefix length in bits
+    num_train = 30;          % Number of training blocks
+    num_preamb_blocks = 3;   % Number of preamble blocks prior to training
+
     % Send multiple training sequences
     x_train = repmat(x_train, 1, num_train);
     
     % Package transmit data
-    tx_cyclic = package_data(x_train, x_data, block_size, prefix_size);
+    tx_cyclic = package_data(x_train, x_data, block_size, prefix_size, num_preamb_blocks);
     
-    preamb_block = sign(randn(1,block_size)) + sign(randn(1,block_size))*1i;
-    preamb = repmat(preamb_block, 1, 3);
-    tx_cyclic = [preamb tx_cyclic];
-    
-    % transmit the signal
+    % Transmit the signal across the channel and process received data.
     y_time = nonflat_channel_timing_error(tx_cyclic);
     output_data =  process_received_data(x_train, tx_cyclic, y_time, block_size, prefix_size, number_of_blocks, num_train);
     
